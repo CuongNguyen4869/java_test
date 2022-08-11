db01040d400d024a1abc3dcd14cd106148b26ac6
CuongNguyen4869 <102643940+CuongNguyen4869@users.noreply.github.com>
2022-08-11 21:41:25 +0700

    Update first_java_file.java

--- basic/first_java_file.java
+++ basic/first_java_file.java
@@ -2,6 +2,6 @@ class first_java_file
 {
     public static void main(String args[])
     {
-        System.out.println("Hello World");
+        System.out.println("Hello World, my name is Cuong");
     }
 }
